89e53e2c922daaf1db6191a611064731584d3b2e
Doug Perez <perezcpt@gmail.com>
2021-02-15 17:23:02 -0800

    Drag prototype

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,48 @@
-var path = require('path');
-var SRC_DIR = path.join(__dirname, '/client/src');
-var DIST_DIR = path.join(__dirname, '/public');
+// var path = require('path');
+// var SRC_DIR = path.join(__dirname, '/client/src');
+// var DIST_DIR = path.join(__dirname, '/public');
+
+// module.exports = {
+//   entry: `${SRC_DIR}/index.jsx`,
+//   output: {
+//     filename: 'bundle.js',
+//     path: DIST_DIR
+//   },
+//   module: {
+//     loaders: [
+//       {
+//         test: /\.jsx?/,
+//         include: SRC_DIR,
+//         loader: 'babel-loader',
+//         query: {
+//           presets: ['react', 'env']
+//         }
+//       }
+//     ]
+//   }
+// };
+
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, '/client/src');
+const OUT_DIR = path.join(__dirname, '/public');
 
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
   output: {
-    filename: 'bundle.js',
-    path: DIST_DIR
+    path: OUT_DIR,
+    filename: 'bundle.js'
   },
   module: {
-    loaders: [
+    rules: [
       {
-        test: /\.jsx?/,
-        include: SRC_DIR,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'env']
-        }
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
       }
     ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
   }
 };
